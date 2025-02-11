e996c0fd27e6ef7ffff036162f787e869794a1ba
psanmartin96 <patricio.sanmartinc95@gmail.com>
2025-02-11 08:58:42 -0300

    Primer commit - Subiendo proyecto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
-export default function RootLayout({
+import { inter } from './ui/fonts';
+import './ui/global.css';
+
+export default function RootLayout ({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
